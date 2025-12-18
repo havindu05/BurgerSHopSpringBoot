d8181dffbfe057552160368703505f3e84244f5b
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-12-18 18:46:47 +0530

    Create customer and item entities

--- src/main/java/edu/icet/model/entity/Customer.java
+++ src/main/java/edu/icet/model/entity/Customer.java
@@ -1,4 +1,26 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Customer {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String phone;
+
+    private String nic;
+
+    private String address;
+
 }
--- src/main/java/edu/icet/model/entity/Item.java
+++ src/main/java/edu/icet/model/entity/Item.java
@@ -1,4 +1,26 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Item {
+
+    @Id
+    private String code;
+
+    private String name;
+
+    private String category;
+
+    private double unitPrice;
+
+    private int qtyOnHand;
+
 }
